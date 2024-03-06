a7beec197664cbf2bb106154839ac5395c201684
srinathlaka <srinathlaka1@gmail.com>
2024-03-06 12:03:22 +0100

    add anaysis functions

--- analysis_functions.py
+++ analysis_functions.py
@@ -3,16 +3,51 @@
 """
 A function to analyze the given parameters and return insights.
 """
+
 def analysis_fit(inputs):
     """
-    Create a directory for the experiment.
+    Create a folder if it doesn't exist and set the folder_data_n variable.
 
-    Parameters: inputs (dict): A dictionary containing keys 'folderdData' and 'expN'.
+    Args:
+    inputs (dict): A dictionary containing input parameters.
 
-    Returns: None
+    Returns:
+    None
     """
-    os.makedirs(os.path.join(inputs['folderdData'], str(inputs['expN'])), exist_ok=True)
-    folder_data_n = os.path.join(inputs['folderdData'], str(inputs['expN']))
+    # Create folder if it doesn't exist
+    folder_path = os.path.join(inputs['folderdData'], str(inputs['expN']))
+    os.makedirs(folder_path, exist_ok=True)
+
+    # Set the folder_data_n variable
+    folder_data_n = folder_path
+
+    # Set time unit (0 for seconds, 1 for minutes, 2 for hours)
+    time_unit = 0  # TODO: Define time unit based on the project requirements
+
+    # Check if background subtraction file exists
+    if os.path.isfile(os.path.join(folder_data_n, 'background_subtracted.npy')):
+        bkg = {
+            'bkg_subract': 1,
+            'filenames': [os.path.join(inputs['folderData'], inputs['fileDataBkg'])],
+            'wells': inputs['bkgWells'],
+            'tin': [[]],
+            'tfin': [[]],
+            'bkgAnalysis': 1,
+            'minut': time_unit
+        }
+        if not os.path.isfile(os.path.join(folder_data_n, 'bkg.npy')):
+            # Compute background
+            bkg['a0'] = 1e-8
+            bkg['n'] = 2
+            bkg['k0'] = []
+            bkg['b0'] = []
+            # Run background computation function
+            bkg_exp = find_bkg(bkg)
+            # Save background to file
+            np.save(os.path.join(folder_data_n, 'bkg.npy'), bkg)
+        else:
+            # Load background from file
+            bkg = np.load(os.path.join(folder_data_n, 'bkg.npy'), allow_pickle=True).item()
 
 
 def analysis_parameters(parameter_analysis):
